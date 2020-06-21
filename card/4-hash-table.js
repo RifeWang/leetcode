5f4868b9423e8352bf573df8e3b88ad86449b88c
RifeWang <rifewang@gmail.com>
2020-06-21 19:51:48 +0800

    hash-table

--- card/4-hash-table.js
+++ card/4-hash-table.js
@@ -465,9 +465,249 @@ var isValidSudoku = function(board) {
  * @return {TreeNode[]}
  */
 var findDuplicateSubtrees = function(root) {
+    if (root == null) {
+        return [];
+    }
 
+    const subtrees = [];
+    const hashmap = new Map();
+    function serialize (root) {
+        if (root == null) {
+            return '';
+        }
+
+        const s = `${root.val}(${serialize(root.left)},${serialize(root.right)})`;
+        if (hashmap.has(s)) {
+            if (hashmap.get(s) === 1) {
+                subtrees.push(root);
+                hashmap.set(s, 2);
+            }
+        } else {
+            hashmap.set(s, 1);
+        }
+        return s;
+    }
+    serialize(root);
+
+    return subtrees;
 };
 
 
 
 
+
+// ----------- 宝石与石头 -----------
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    if (J.length == 0 || S.length == 0) {
+        return 0;
+    }
+
+    let result = 0;
+    const hashmap = new Map();
+    J.split('').map(v => hashmap.set(v, 0));
+    for (let i = 0; i < S.length; i++) {
+        const v = S.charAt(i);
+        if (hashmap.has(v)) {
+            result++;
+        }
+    }
+    return result;
+};
+
+
+
+
+
+// ----------- 无重复字符的最长子串 -----------
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    if (s.length == 0) {
+        return 0;
+    }
+    if (s.length == 1) {
+        return 1;
+    }
+
+    let max = 1;
+    let sub = s.charAt(0);
+    for (let i = 1; i < s.length; i++) {
+        const item = s.charAt(i);
+
+        if (sub.indexOf(item) == -1) {
+            sub += item;
+        } else {
+            sub = sub.slice(sub.indexOf(item)+1) + item;
+        }
+        max = Math.max(sub.length, max);
+    }
+
+    return max;
+};
+
+
+
+
+
+// -----------  四数相加 II -----------
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @param {number[]} C
+ * @param {number[]} D
+ * @return {number}
+ */
+var fourSumCount = function(A, B, C, D) {
+    let result = 0;
+
+    const mapC = new Map();
+    C.map(v => {
+        if (mapC.has(v)) {
+            mapC.set(v, mapC.get(v) + 1);
+        } else {
+            mapC.set(v, 1);
+        }
+    });
+
+    function dfs(path) {
+        if (path.length == 2) {
+            const sum = path.reduce((acc, cur) => acc+cur, 0);
+            const target = -sum;
+
+            // C + D = -sum
+            let count = 0;
+            D.map(v => {
+                const targetC = -sum-v;
+                if (mapC.has(targetC)) {
+                    count += mapC.get(targetC);
+                }
+            });
+
+            result += count;
+        }
+        if (path.length == 0) {
+            for (let i = 0; i < A.length; i++) {
+                path.push(A[i]);
+                dfs(path);
+                path.pop();
+            }
+        }
+        if (path.length == 1) {
+            for (let i = 0; i < B.length; i++) {
+                path.push(B[i]);
+                dfs(path);
+                path.pop();
+            }
+        }
+    }
+    dfs([]);
+
+    return result;
+};
+
+
+
+
+
+// -----------  前 K 个高频元素 -----------
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    nums.sort((a, b) => a-b);
+    const result = [];
+    const map = new Map();
+    for (let i = 0; i < nums.length; ) {
+        const item = nums[i];
+        const itemCount = nums.lastIndexOf(item) - nums.indexOf(item) + 1;
+        map.set(item, itemCount);
+        i = nums.lastIndexOf(item) + 1;
+        if (result.length < k) {
+            result.push(item);
+        } else {
+            result.sort((a, b) => map.get(a) - map.get(b));
+            if (map.get(result[0]) < itemCount) {
+                result.shift();
+                result.push(item);
+            }
+        }
+    }
+    return result;
+};
+
+
+
+
+
+// -----------  常数时间插入、删除和获取随机元素 -----------
+/**
+ * Initialize your data structure here.
+ */
+var RandomizedSet = function() {
+    // 数组存储元素，map 存储元素对应索引，删除时需要将元素与数组尾部交换然后删除尾部
+    this._map = new Map();
+    this._arr = [];
+};
+
+/**
+ * Inserts a value to the set. Returns true if the set did not already contain the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this._map.has(val)) {
+        return false;
+    } else {
+        this._arr.push(val);
+        this._map.set(val, this._arr.length-1);
+        return true;
+    }
+};
+
+/**
+ * Removes a value from the set. Returns true if the set contained the specified element.
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if (this._map.has(val)) {
+        const index = this._map.get(val);
+        this._map.set(this._arr[this._arr.length-1], index);
+        [this._arr[index], this._arr[this._arr.length-1]] = [this._arr[this._arr.length-1], this._arr[index]];
+
+        this._map.delete(val);
+        this._arr.pop();
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/**
+ * Get a random element from the set.
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    if (this._arr.length == 0) {
+        return null;
+    }
+    const random = Math.floor(Math.random() * this._arr.length);
+    return this._arr[random];
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
